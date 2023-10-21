9ce0ac645b81fb40c9d8467a68c857626b3eb76b
compermane <comper.mane@gmail.com>
2023-10-21 14:53:13 -0300

    Scripts para automação e testagem da ferramenta FlaPy

--- FlapyRepos/flapper.py
+++ FlapyRepos/flapper.py
@@ -1,18 +1,27 @@
 from main import *
 from datetime import date
+import subprocess
+import os
 
 CSV_REPOS = 'repos_csv.csv'
+PRE_REPOS = 'pre_repos.csv'
+REPOS_TO_CLONE = 'flaky_repos.csv'
+REPOS_TO_FLAPY = 'repos_to_flapy.csv'
 
 if __name__ == '__main__':
     data = date.today()
     directory = 'FlaPy-Repos-' + str(data)
-    comando = 'mkdir ' + directory
-    os.system(comando)
+    subprocess.run(["mkdir", directory])
 
-    writer()
-    repos = dict()
-    repos = reader(CSV_REPOS)
-    cloning(repos)
+    preRepos = dict()
+    preRepos = reader(REPOS_TO_CLONE)
 
-    numRuns = getNumRuns(repos)
-    comando = './flapy.sh run --plus-random-runs --out-dir ' + directory + CSV_REPOS + str(numRuns)
+    # Executa o flapy para cara repositorio separadamente
+    for hash in preRepos:
+        cwd = os.getcwd()
+        cloning(preRepos, hash)
+        numRuns = preRepos[hash][2]
+        writer(preRepos[hash])
+        os.chdir(cwd)
+        flapper(directory, REPOS_TO_FLAPY, numRuns)
+        depipping(preRepos, hash)
--- FlapyRepos/main.py
+++ FlapyRepos/main.py
@@ -1,33 +1,46 @@
 import csv
 import os
 import random
+import subprocess
 
-def cloning(repos: dict()) -> None:
-    os.chdir(os.getcwd() + '/Repos')
-    for hash in repos: 
-        nome = repos[hash][0]
-        url = repos[hash][1]
-        comando = 'git clone ' + url
-        os.system(comando)
-        cwd = os.getcwd()
-        os.chdir(cwd + "/" + nome)
-        comando = 'git checkout ' + hash
-        os.system(comando)
-        pipping()
-        os.chdir(cwd)
+def cloning(repos: dict(), hash: str) -> None:
+    cwd = os.getcwd() + "/Repos"
+    os.chdir(cwd)
+    nome = repos[hash][0]
+    url = repos[hash][1]
+
+    # Roda o git clone
+    subprocess.run(["git", "clone", url])
+
+    # Entra na pasta do repositorio clonado
+    os.chdir(cwd + "/" + nome)
+
+    # Faz o checkout para o commit da hash
+    subprocess.run(["git", "checkout", hash])
+
+    pipping()
 
 def pipping() -> None:
-    comando = 'pip install -r requirements.txt'
-    try:
-        os.system(comando)
-    except OSError as e:
-        print(f"Nao foi possivel instalar as dependencias: {e}")
+    subprocess.run(["pip", "install", "-r", "requirements.txt"])
 
-def depipping() -> None:
-    comand = 'pip uninstal -r requirements.txt -Y'
+def depipping(repos: dict(), hash: str) -> None:
+    comando = 'pip uninstall -r requirements.txt -y'
+    cwd = os.getcwd()
 
+    nome = repos[hash][0]
+    os.chdir(os.getcwd() + "/Repos/" + nome)
+    os.system(comando)
+    os.chdir(cwd)
+
+def flapper(directory: str, csv: str, numRuns: int) -> None:
+    subprocess.Popen("./flapy.sh run --plus-random-runs --out-dir %s %s %s" % (str(directory), str(csv), str(numRuns)), shell = True)
+
+"""
+    Retorna um dicionario contendo informacoes sobre o nome do projeto, URL do projeto
+    e numero de runs de cada repositorio do artigo do FlaPy. 
+"""
 def reader(csv_name: str) -> dict():
-    os.system('mkdir Repos')
+    subprocess.run(["mkdir", "Repos"])
     with open(csv_name, 'r') as csv_flapy:
         repos = dict()
         csv_reader = csv.DictReader(csv_flapy, delimiter = ',')
@@ -39,28 +52,43 @@ def reader(csv_name: str) -> dict():
 
     return repos
 
-def writer() -> None:
-    with open('repos_csv.csv', 'a', newline = '') as repos_csv:
-        writer =  csv.writer(repos_csv)
-        header = ["Project_Hash", "Project_Name", "Project_URL", "Num_Runs"]
+"""
+    Escreve um csv de acordo com o modo:
+    1: escolher os repositorios de TestsOveview
+    2: montar um csv com os repositorios ja escolhidos
+"""
+def writer(repos = tuple) -> None:
+    with open('repos_to_flapy.csv', 'w', encoding = "utf8") as toFlapy:
+        writer = csv.writer(toFlapy)
+        header = ["Project_Name", "Project_URL", "Project_Hash", "Num_Runs"]
         writer.writerow(header)
 
-        with open('TestsOverview.csv', 'r', encoding = "utf8") as flapy_repos:
-            reader = csv.reader(flapy_repos)
+        row = [repos[0], "./Repos/" + repos[0], repos[1], repos[2]]
+        writer.writerow(row)
 
-            randRepo = random.choice(list(reader))
-            name = randRepo[0]
-            url = randRepo[1]
-            hash = randRepo[2]
-            runs = randRepo[11]
-            writer.writerow([hash, name, url, runs])
+        toFlapy.close()
 
-            flapy_repos.close()
+"""
+    Funcao para obter os repositorios com pelo menos 1 flaky test
+"""
+def getFlakyRepos() -> None:
+    with open("TestsOverview.csv", "r", encoding = "utf8") as flapy_csv:
+        reader = csv.DictReader(flapy_csv, delimiter = ",")
+        flaky_repos = dict()
 
-        repos_csv.close()
+        for row in reader:
+            if (row["Verdict_sameOrder"] == "Flaky" or row["Verdict_randomOrder"] == "Flaky") and row["Project_Hash"] not in flaky_repos:
+                flaky_repos[row["Project_Hash"]] = (row["Project_Name"], row["Project_URL"], row["Project_Hash"], row["#Runs_sameOrder"])
 
-def getNumRuns(repos: dict()) -> int:
-    for hash in repos:
-        num = repos[hash][2]
+        flapy_csv.close()
 
-    return num
+    with open("flaky_repos.csv", "w", newline = '') as flapy_csv:
+        writer = csv.writer(flapy_csv)
+        header = ["Project_Name", "Project_URL", "Project_Hash", "Num_Runs"]
+        writer.writerow(header)
+
+        for hash in flaky_repos:
+            row = [flaky_repos[hash][0], flaky_repos[hash][1], flaky_repos[hash][2], flaky_repos[hash][3]]
+            writer.writerow(row)
+
+        flapy_csv.close()
