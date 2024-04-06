782950f761c06719d03d9f37cf76eb4e6b6025cc
compermane <comper.mane@gmail.com>
2024-04-06 20:23:42 -0300

    Documentação das funções

--- FlapyRepos/utils.py
+++ FlapyRepos/utils.py
@@ -1,8 +1,14 @@
 import csv
 import os
 import subprocess
+from typing import Dict, Tuple
 
-def cloning(repos: dict(), hash: str) -> None:
+def cloning(repos: Dict, hash: str) -> None:
+    """ (descontinuado) Realiza o cloning de um repositório, dado o dicionário de repositórios e sua respectiva chave.
+    :param repos: dicionário de repositórios
+    :param hash: hash do repositório a ser clonado
+    :returns: None
+    """
     cwd = os.getcwd() + "/Repos"
     os.chdir(cwd)
     nome = repos[hash][0]
@@ -20,9 +26,17 @@ def cloning(repos: dict(), hash: str) -> None:
     pipping()
 
 def pipping() -> None:
+    """ (descontinuado) Realiza a instalação das dependências de um repositório. Recomendado utilizar junto com a função cloning()
+    :returns: None
+    """
     subprocess.run(["pip", "install", "-r", "requirements.txt"])
 
-def depipping(repos: dict(), hash: str) -> None:
+def depipping(repos: Dict, hash: str) -> None:
+    """ (descontinuado) Realiza a desinstalação das dependências de um repositório.
+    :param repos: dicionário de repositórios
+    :param hash: hash do repositório dentro do dicionário
+    :returns: None
+    """
     comando = 'pip uninstall -r requirements.txt -y'
     cwd = os.getcwd()
 
@@ -32,14 +46,20 @@ def depipping(repos: dict(), hash: str) -> None:
     os.chdir(cwd)
 
 def flapper(directory: str, csv: str, numRuns: int) -> None:
+    """ Realiza a execução do FlaPy.
+    :param directory: diretório de saída dos resultados
+    :param csv: arquivo .csv contendo os repositórios de teste
+    :param numRuns: número de execuções dos testes do repositório
+    :returns: None
+    """
     p1 = subprocess.Popen("./flapy.sh run --plus-random-runs --out-dir %s %s %s" % (str(directory), str(csv), str(numRuns)), shell = True)
     p1.wait()
 
-"""
-    Retorna um dicionario contendo informacoes sobre o nome do projeto, URL do projeto
-    e numero de runs de cada repositorio do artigo do FlaPy. 
-"""
-def reader(csv_name: str) -> dict():
+def reader(csv_name: str) -> Dict:
+    """ Realiza a leitura de um arquivo .csv e retorna informações dos repositórios contidos nele.
+    :param csv_name: nome do arquivo .csv
+    :returns: Dict, o qual cada chave é o GitHash do repositório que relaciona a uma tupla contendo o nome do repositório, sua URL e a quantidade de execuções segundo o FlaPy
+    """
     subprocess.run(["mkdir", "Repos"])
     with open(csv_name, 'r') as csv_flapy:
         repos = dict()
@@ -52,12 +72,12 @@ def reader(csv_name: str) -> dict():
 
     return repos
 
-"""
-    Escreve um csv de acordo com o modo:
-    1: escolher os repositorios de TestsOveview
-    2: montar um csv com os repositorios ja escolhidos
-"""
-def writer(repos, hash) -> None:
+def writer(repos: Tuple, hash: str) -> None:
+    """ Realiza a escrita de um arquivo .csv contendo informações de um repositório para a execução do FlaPy
+    :param repos: tupla contendo informação do repositório
+    :param hash: hash do repositório
+    :returns: None
+    """
     with open('repos_to_flapy.csv', 'w', newline = '') as toFlapy:
         writer = csv.writer(toFlapy)
         header = ["PROJECT_NAME", "PROJECT_URL", "PROJECT_HASH", "PYPI_TAG", "FUNCS_TO_TRACE", "TESTS_TO_BE_RUN", "NUM_RUNS"]
@@ -68,10 +88,10 @@ def writer(repos, hash) -> None:
 
         toFlapy.close()
 
-"""
-    Funcao para obter os repositorios com pelo menos 1 flaky test
-"""
 def getFlakyRepos() -> None:
+    """ Escreve em um .csv os repositórios que contém pelo menos 1 flaky test.
+    :returns: None
+    """
     with open("TestsOverview.csv", "r", encoding = "utf8") as flapy_csv:
         reader = csv.DictReader(flapy_csv, delimiter = ",")
         flaky_repos = dict()
@@ -93,12 +113,22 @@ def getFlakyRepos() -> None:
 
         flapy_csv.close()
 
-def writeLog(repo_name: str, time_taken: float, observacoes = "Nenhuma") -> None:
+def writeLog(repo_name: str, time_taken: float, observacoes: str = "Nenhuma") -> None:
+    """ Escreve um "log", contendo informações sobre a execução do FlaPy sobre um determinado repositório.
+    :param repo_name: nome do repositório
+    :param time_taken: tempo de execução do FlaPy (em horas)
+    :param observacoes: observação da execução, caso haja
+    :returns: None
+    """
     with open("log.txt", "a", encoding = "utf8") as logFile:
         print(f"{repo_name}: {time_taken} Observacoes: {observacoes}", file = logFile)
         logFile.close()
 
 def getNonOrderDependentRepos(csv_repos: str) -> None:
+    """ Escreve, em um arquivo .csv, repositórios que possuam pelo menos 1 teste flaky NOD
+    :param csv_repos: .csv inicial contendo os repositórios e os vereditos dos testes
+    :returns: None
+    """
     with open(csv_repos, "r", encoding = "utf8") as csvFile:
         reader = csv.DictReader(csvFile, delimiter = ",")
         repos = dict()
@@ -121,6 +151,11 @@ def getNonOrderDependentRepos(csv_repos: str) -> None:
         csvFile.close()
 
 def diff(csv1: str, csv2: str) -> None:
+    """ (descontinuado) Realiza a diferença entre dois .csv e escreve em outro .csv
+    :param csv1: uma das planilhas
+    :param csv2: a outra planilha
+    :returns: None
+    """
     repos1 = dict()
     repos1 = reader(csv1)
 
