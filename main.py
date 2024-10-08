16426f9a14c78b89f108330093894bacb4cc619a
compermane <comper.mane@gmail.com>
2024-10-08 12:25:52 -0300

    main

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-from Analise import analise, utils
+from Analise import analise, utils, VirtualEnvironment
 from os import chdir, getcwd, path
 from typing import List
+from time import sleep
 import argparse
+import csv
+import subprocess
 
 def str_to_bool(value: str) -> bool:
     if value.lower() in {"true", "t"}:
@@ -21,14 +24,16 @@ def argsDefiner():
     parser = argparse.ArgumentParser()
 
     # Adicionando parâmetros command-line
-    parser.add_argument("--repo_dir", help = "Diretorio para o repositorio", type = str, default = "")
-    parser.add_argument("--repo_name", help = "Nome do repositorio", type = str, default = "")
+    parser.add_argument("--repo-dir", help = "Diretorio para o repositorio", type = str, default = "")
+    parser.add_argument("--repo-name", help = "Nome do repositorio", type = str, default = "")
     parser.add_argument("--read-from-csv", help = "CSV com informações do repositório a ser lido", type = str, default = "")
     parser.add_argument("--output-dir", help = "Especificar o diretorio para onde os resultados de teste serao armazenados", default =  ".")
     parser.add_argument("--no-runs", help =  "Especificar a quantidade de rodadas que cada repositorio tera", type = str_to_int, default = 1)
     parser.add_argument("--include-test-tracing", help = "Determinar se a ferramenta deve executar o tracing de cada teste. O default eh True", type = str_to_bool, default = True)
     parser.add_argument("--include-test-coverage", help = "Determinar se a ferramenta deve executar o coverage de cada teste. O default eh False", type = str_to_bool, default = False)
-    parser.add_argument("--include-test-profiling", help = "DEterminar se a ferramente deve realizar o profiling dos testes. O default eh False", type = str_to_bool, default = False)
+    parser.add_argument("--include-test-profiling", help = "Determinar se a ferramente deve realizar o profiling dos testes. O default eh False", type = str_to_bool, default = False)
+    parser.add_argument("--run-specific-test", help = "Rodar a ferramenta em testes específicos. Deve-se ser informados, em um csv, o nome do repositório, sua URL, seu GitHash, o node do teste e a quantidade de execuções.", type = str, default = "")
+    parser.add_argument("--venv-dir", help = "Virtual environment associado a reprodução dos resultados", type = str, default = True)
 
     # Adicionando os parametros
     args = parser.parse_args()
@@ -37,15 +42,43 @@ def argsDefiner():
     tracing = args.include_test_tracing
     coverage = args.include_test_coverage
     profiling = args.include_test_profiling
+    specificTests = args.run_specific_test
+    venvDir = args.venv_dir
 
-    if csvFile == "":
+    if csvFile == "" and specificTests == "":
         # Obtendo os parametros especificados
         repo = args.repo_dir
         name = args.repo_name
         noruns = args.no_runs
 
         analise.runMultipleTimes(repo, name, noruns, [tracing, coverage, profiling])
-    else:
+    elif specificTests != "":
+        if venvDir == "":
+            raise ValueError("Nao foi informado um diretorio para o ambiente virtual")
+        else:
+            with open(specificTests, "r", encoding = "utf8") as csv_file:
+
+                reader = csv.DictReader(csv_file,  delimiter = ",")
+                for row in reader:
+                    # Instalando as dependências desse projeto
+                    process = subprocess.Popen(
+                        "pip3 install --force-reinstall -r requirements.txt", stdout = subprocess.PIPE, stderr = subprocess.PIPE, 
+                        shell = True, executable = "/bin/bash"
+                    )
+                    process.wait()
+                    
+                    repo_name = row["Name"]
+                    repo_hash = row["Hash"]
+                    repo_url = row["URL"]
+                    test_no_runs = int(float(row["No_Runs"]))
+                    test_node = row["Test"]
+
+                    repo = utils.Repository(githash = repo_hash, url = repo_url, isgitrepo = True, noruns = str(test_no_runs))
+
+                    utils.runSpecificTests(repo, repo_name, [tracing, coverage, profiling], test_node, test_no_runs, env_path = venvDir)
+                    sleep(10)
+
+    elif csvFile != "" and specificTests == "":
         repos: List[utils.Repository] = utils.readCSV(csvFile)
         for repo in repos:
             utils.cloning(repo)
@@ -76,10 +109,3 @@ def test_profiler() -> None:
 
 if __name__ == "__main__":
     argsDefiner()
-    # analise.traceDiff("Test-analytic_shrinkage")
-    # analise.flakyFinder("Test-analytic_shrinkage")
-    # print(path.abspath("analytic_shrinkage"))
-    # print(ignoreSpaces(path.abspath("analytic_shrinkage")))
-    # bruh = utils.Repository("analytic_shrinkage", 100)
-    # utils.venving(bruh)
-    # utils.activating(bruh)
